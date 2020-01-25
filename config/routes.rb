45f7c7a83d09337d2c687dc4d057ce27424e2b19
koi-chan <koi-chan@mail.kazagakure.net>
2020-01-25 17:21:01 +0900

    rails g controller Images index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
